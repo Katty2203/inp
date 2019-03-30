0791a5622aefec33729c86183c48ff4a42b2418e
Katty2203 <44648577+Katty2203@users.noreply.github.com>
2019-03-30 13:28:36 +0200

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,10 @@
+function sendQuestion(){
+
+var inputs, index;
+
+inputs = document.getElementsByTagName('input');
+for (index = 0; index < inputs.length; ++index) {
+    console.log(inputs[index].value);
+    }
+
+}
